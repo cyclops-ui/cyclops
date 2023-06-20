40d52799d8b98f3fbd76cd6e84001777123c0c62
Petar Cvitanović <petar.cvitanovic@superbet.com>
2023-06-20 16:08:20 +0200

    remove deprecated ctrl

--- cyclops-ctrl-deprecated/internal/models/dto/modules.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-type CreateModuleRequest struct {
-	Name     string                 `json:"name"`
-	Template string                 `json:"template"`
-	Values   map[string]interface{} `json:"values"`
-}
