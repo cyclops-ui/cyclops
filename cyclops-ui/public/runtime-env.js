c31744916e30fa87eb2999a4560b7ed32439b90f
Petar Cvitanović <72022639+petar-cvit@users.noreply.github.com>
2023-09-06 18:30:08 +0200

    Add default env vars

--- cyclops-ui/public/runtime-env.js
+++ cyclops-ui/public/runtime-env.js
@@ -1,4 +1,7 @@
 window.__RUNTIME_CONFIG__ = {
     "NODE_ENV": "development",
-    "REACT_APP_CYCLOPS_CTRL_HOST": "http://localhost:8888"
+    "REACT_APP_CYCLOPS_CTRL_HOST": "http://localhost:8888",
+    "DEFAULT_TEMPLATE_REPO": "",
+    "DEFAULT_TEMPLATE_PATH": "",
+    "DEFAULT_TEMPLATE_VERSION": ""
 };
