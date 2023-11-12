a9c220eb280bcdc2dca322791ff992195ef60593
Petar Cvitanović <petar.cvitanovic@superbet.com>
2023-11-12 19:24:33 +0100

    bump docusaurus version

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -1,9 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Cyclops',
@@ -144,10 +141,6 @@ const config = {
             ]
           }
         ],
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
       },
       colorMode: {
         defaultMode: 'light',
