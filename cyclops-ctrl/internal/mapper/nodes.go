0743dd7447c462781116b7069d67c468c7bd6d21
Petar Cvitanović <petar.cvitanovic@superbet.com>
2023-11-18 19:26:25 +0100

    ctrl remove dead code

--- cyclops-ctrl/internal/mapper/nodes.go
+++ cyclops-ctrl/internal/mapper/nodes.go
@@ -1,10 +1,6 @@
 package mapper
 
 import (
-	"fmt"
-	"math"
-	"strconv"
-
 	"github.com/cyclops-ui/cycops-ctrl/internal/models/dto"
 	apiv1 "k8s.io/api/core/v1"
 )
@@ -89,34 +85,4 @@ func mapNodePods(pods []apiv1.Pod) []dto.NodePod {
 	}
 
 	return out
-}
-
-func Round(val float64, roundOn float64, places int) (newVal float64) {
-	var round float64
-	pow := math.Pow(10, float64(places))
-	digit := pow * val
-	_, div := math.Modf(digit)
-	if div >= roundOn {
-		round = math.Ceil(digit)
-	} else {
-		round = math.Floor(digit)
-	}
-	newVal = round / pow
-	return
-}
-
-func humanizeMemory(size float64) string {
-	var suffixes [5]string
-
-	suffixes[0] = "B"
-	suffixes[1] = "KB"
-	suffixes[2] = "MB"
-	suffixes[3] = "GB"
-	suffixes[4] = "TB"
-
-	base := math.Log(size) / math.Log(1024)
-	getSize := Round(math.Pow(1024, base-math.Floor(base)), .5, 2)
-	fmt.Println(int(math.Floor(base)))
-	getSuffix := suffixes[int(math.Floor(base))]
-	return strconv.FormatFloat(getSize, 'f', -1, 64) + " " + string(getSuffix)
 }
