c763510dd536e9f53f3dba83e87378224bb99a89
Petar Cvitanović <petar.cvitanovic@superbet.com>
2023-12-17 12:50:27 +0100

    :ok_hand: number validations

--- cyclops-ui/public/env-config.js
+++ cyclops-ui/public/env-config.js
@@ -2,6 +2,6 @@ window.__RUNTIME_CONFIG__ = {
     "NODE_ENV": "development",
     "REACT_APP_CYCLOPS_CTRL_HOST": "http://localhost:8888",
     "REACT_APP_DEFAULT_TEMPLATE_REPO": "https://github.com/cyclops-ui/templates",
-    "REACT_APP_DEFAULT_TEMPLATE_PATH": "demo",
-    "REACT_APP_DEFAULT_TEMPLATE_VERSION": "chart-deps"
+    "REACT_APP_DEFAULT_TEMPLATE_PATH": "test",
+    "REACT_APP_DEFAULT_TEMPLATE_VERSION": ""
 };
