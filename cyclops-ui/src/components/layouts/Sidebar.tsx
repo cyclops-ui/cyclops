3016932aaeefd836c2babd7b70eff261e75b5c2d
Juraj Karadža <karadza@Jurajs-MacBook-Pro.local>
2024-01-03 19:52:31 +0100

    simplify the sidebar menu and fix the current tab tracking

--- cyclops-ui/src/components/layouts/Sidebar.tsx
+++ cyclops-ui/src/components/layouts/Sidebar.tsx
@@ -4,51 +4,37 @@ import {
     AppstoreAddOutlined,
     HddOutlined,
 } from '@ant-design/icons';
-import {useNavigate}  from 'react-router';
+import {useLocation} from 'react-router';
 import PathConstants from "../../routes/PathConstants";
 import { Link } from 'react-router-dom';
 
+
 const SideNav = () => {
-    const navigate = useNavigate();
-    // const handleModulesClick = () => {
-    //     history('/modules');
-    // }
-    // const handleNodesClick = () => {
-    //     history('/nodes');
-    // }
+    const location = useLocation();
+    const locationKey = location.pathname.split('/', 2).join('/')
 
     const sidebarItems: MenuProps['items'] = [{
         label: <Link to={PathConstants.MODULES}> Modules</Link>,
         icon: <AppstoreAddOutlined/>,
-        key: 'modules'
+        key: '/modules'
     }, {
         label: <Link to={PathConstants.NODES}> Nodes</Link>,
         icon: <HddOutlined/>,
-        key: 'nodes'
+        key: '/nodes'
     }];
 
 
 
     return (
         <div>
-            <a href={PathConstants.HOME}>
+            <a href={PathConstants.MODULES}>
                 <div style={{height: "32px", width: "70%", margin: "0.9rem 1rem 0.6rem 2rem", display: "inline-flex"}}>
                     <h2 style={{color: "white", marginTop: "5px"}}><b>Cyclops</b></h2>
                     <img  style={{height: "120%", marginLeft: "6px"}}
                          src={require("./KIKLOPcic.png")} alt="Cyclops" />
                 </div>
             </a>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['modules']} items={sidebarItems}/>
-            {/*<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>*/}
-            {/*    <Menu.Item key="1" onClick={handleModulesClick}>*/}
-            {/*        <AppstoreAddOutlined />*/}
-            {/*        <span> Modules</span>*/}
-            {/*    </Menu.Item>*/}
-            {/*    <Menu.Item key="2" onClick={handleNodesClick}>*/}
-            {/*        <HddOutlined />*/}
-            {/*        <span> Nodes</span>*/}
-            {/*    </Menu.Item>*/}
-            {/*</Menu>*/}
+            <Menu theme="dark" mode="inline" selectedKeys={[locationKey]} items={sidebarItems}/>
         </div>
     );
 }
