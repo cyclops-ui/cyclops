23e1fdbf45acefc57aa9067e64c56d10d8405d5e
petar-cvit <petar.cvitanovic@gmail.com>
2024-01-05 19:11:54 +0100

    ✨ check pod age when formatting

--- cyclops-ui/src/utils/pods.tsx
+++ cyclops-ui/src/utils/pods.tsx
@@ -1,6 +1,10 @@
 import {formatDistanceToNow} from "date-fns";
 
-function formatPodAge(podAge: string): string {
+export function formatPodAge(podAge: string): string {
+    if (podAge === "") {
+        return ""
+    }
+
     const parsedDate = new Date(podAge);
     return formatDistanceToNow(parsedDate, { addSuffix: true });
 }
