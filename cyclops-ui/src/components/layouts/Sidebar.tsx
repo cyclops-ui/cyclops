0dbbc0aad997ffcbd0d7a91ad47c8219bb239459
petar-cvit <petar.cvitanovic@gmail.com>
2024-03-06 22:17:49 +0100

    🧐 display current version

--- cyclops-ui/src/components/layouts/Sidebar.tsx
+++ cyclops-ui/src/components/layouts/Sidebar.tsx
@@ -22,45 +22,52 @@ const SideNav = () => {
   ];
 
   return (
-    <div
-      style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
-    >
-      <a href={PathConstants.MODULES}>
-        <div
-          style={{
-            height: "32px",
-            width: "70%",
-            margin: "0.9rem 1rem 0.6rem 2rem",
-            display: "inline-flex",
-          }}
-        >
-          <h2 style={{ color: "white", marginTop: "5px" }}>
-            <b>Cyclops</b>
-          </h2>
-          <img
-            style={{ height: "120%", marginLeft: "6px" }}
-            src={require("./KIKLOPcic.png")}
-            alt="Cyclops"
-          />
-        </div>
-      </a>
-      <Menu
-        theme="dark"
-        mode="inline"
-        selectedKeys={[location]}
-        items={sidebarItems}
-      />
-      <Button
-        ghost
-        style={{ margin: "auto 25px 25px 25px" }}
-        icon={<BugFilled />}
-        href={
-          "https://github.com/cyclops-ui/cyclops/issues/new?assignees=&labels=&projects=&template=bug_report.md&title="
-        }
+      <div
+          style={{display: "flex", flexDirection: "column", minHeight: "100vh"}}
       >
-        <b>Report a Bug</b>
-      </Button>
-    </div>
+        <a href={PathConstants.MODULES}>
+          <div
+              style={{
+                height: "32px",
+                width: "70%",
+                margin: "0.9rem 1rem 0.6rem 2rem",
+                display: "inline-flex",
+              }}
+          >
+            <h2 style={{color: "white", marginTop: "5px"}}>
+              <b>Cyclops</b>
+            </h2>
+            <img
+                style={{height: "120%", marginLeft: "6px"}}
+                src={require("./KIKLOPcic.png")}
+                alt="Cyclops"
+            />
+          </div>
+        </a>
+        <Menu
+            theme="dark"
+            mode="inline"
+            selectedKeys={[location]}
+            items={sidebarItems}
+        />
+        <Button
+            ghost
+            style={{margin: "auto 25px 25px 25px"}}
+            icon={<BugFilled/>}
+            href={
+              "https://github.com/cyclops-ui/cyclops/issues/new?assignees=&labels=&projects=&template=bug_report.md&title="
+            }
+        >
+          <b>Report a Bug</b>
+        </Button>
+        <center style={{
+          color: "#FFF",
+          margin: "25px",
+          marginTop: "0",
+        }}>
+          {window.__RUNTIME_CONFIG__.REACT_APP_VERSION}
+        </center>
+      </div>
   );
 };
 export default SideNav;
