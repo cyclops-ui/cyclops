c093c03bd2ff2e1977d6e13cd4446b7128c97121
petar-cvit <petar.cvitanovic@gmail.com>
2024-03-17 23:06:47 +0100

    redesign comparison

--- web/src/components/Comparison/index.js
+++ web/src/components/Comparison/index.js
@@ -1,176 +1,231 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './styles.module.css';
-import CodeBlock from '@theme/CodeBlock'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+
 import {ConfigProvider, Form, Input, InputNumber, Switch} from "antd";
 
-class Comparison extends React.Component {
-    constructor(props) {
-        super(props);
+const Comparison = () => {
+    const [name, setName] = useState("nginx");
+    const [replicas, setReplicas] = useState(3);
+    const [version, setVersion] = useState("1.14.2");
+    const [port, setPort] = useState(80);
+    const [expose, setExpose] = useState(false);
 
-        this.state = {
-            name: "nginx",
-            replicas: "3",
-            version: "1.14.2",
-            port: "80",
-            service: false,
-        };
+    const [form] = Form.useForm();
+
+    const typeName = (name) => {
+        setName(name);
+        form.setFieldValue("Name", name)
     }
 
-    render() {
-        const k8sDplString = 'apiVersion: apps/v1\n' +
-            'kind: Deployment\n' +
-            'metadata:\n' +
-            '  name: ' + this.state.name + '\n' +
-            '  labels:\n' +
-            '    app: ' + this.state.name + '\n' +
-            'spec:\n' +
-            '  replicas: ' + this.state.replicas + '\n' +
-            '  selector:\n' +
-            '    matchLabels:\n' +
-            '      app: ' + this.state.name + '\n' +
-            '  template:\n' +
-            '    metadata:\n' +
-            '      labels:\n' +
-            '        app: ' + this.state.name + '\n' +
-            '    spec:\n' +
-            '      containers:\n' +
-            '      - name: ' + this.state.name + '\n' +
-            '        image: ' + this.state.name + ':' + this.state.version + '\n' +
-            '        ports:\n' +
-            '        - containerPort: ' + this.state.port
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            console.log(name)
 
-        const k8sSvcString = '---\n' +
-            'apiVersion: v1\n' +
-            'kind: Service\n' +
-            'metadata:\n' +
-            '  name: ' + this.state.name + '\n' +
-            'spec:\n' +
-            '  selector:\n' +
-            '    app: ' + this.state.name + '\n' +
-            '  ports:\n' +
-            '    - protocol: TCP\n' +
-            '      port: ' + this.state.port + '\n' +
-            '      targetPort: 9376'
+            if (name === "nginx") {
+                setTimeout(() => typeName("ngin"), 0)
+                setTimeout(() => typeName("ngi"), 50)
+                setTimeout(() => typeName("ng"), 100)
+                setTimeout(() => typeName("n"), 150)
+                setTimeout(() => typeName(""), 200)
 
-        const onNameChange = (event) => {
-            this.setState({
-                name: event.target.value,
-                replicas: this.state.replicas,
-                version: this.state.version,
-                port: this.state.port,
-                service: this.state.service,
-            })
-        }
+                setTimeout(() => typeName("m"), 500)
+                setTimeout(() => typeName("my"), 600)
+                setTimeout(() => typeName("my-"), 700)
+                setTimeout(() => typeName("my-a"), 800)
+                setTimeout(() => typeName("my-ap"), 900)
+                setTimeout(() => typeName("my-app"), 1000)
+            } else {
+                setTimeout(() => typeName("my-ap"), 0)
+                setTimeout(() => typeName("my-a"), 50)
+                setTimeout(() => typeName("my-"), 100)
+                setTimeout(() => typeName("my"), 150)
+                setTimeout(() => typeName("m"), 200)
+                setTimeout(() => typeName(""), 250)
 
-        const onReplicasChange = (replicas) => {
-            this.setState({
-                name: this.state.name,
-                replicas: replicas,
-                version: this.state.version,
-                port: this.state.port,
-                service: this.state.service,
-            })
-        }
-
-        const onVersionChange = (event) => {
-            this.setState({
-                name: this.state.name,
-                replicas: this.state.replicas,
-                version: event.target.value,
-                port: this.state.port,
-                service: this.state.service,
-            })
-        }
-
-        const onPortChange = (port) => {
-            this.setState({
-                name: this.state.name,
-                replicas: this.state.replicas,
-                version: this.state.version,
-                port: port,
-                service: this.state.service,
-            })
-        }
-
-        const onNeedService = (service) => {
-            this.setState({
-                name: this.state.name,
-                replicas: this.state.replicas,
-                version: this.state.version,
-                port: this.state.port,
-                service: service,
-            })
-        }
-
-        const getManifest = () => {
-            var manifest = k8sDplString;
-
-            if (this.state.service) {
-                manifest += '\n' + k8sSvcString;
+                setTimeout(() => typeName("n"), 500)
+                setTimeout(() => typeName("ng"), 600)
+                setTimeout(() => typeName("ngi"), 700)
+                setTimeout(() => typeName("ngin"), 800)
+                setTimeout(() => typeName("nginx"), 900)
             }
+        }, 2000);
 
-            return manifest
+        return () => clearInterval(intervalId);
+    }, [name])
+
+    const mapReplicas = (n) => {
+        if (n === null || n === undefined) {
+            return 0
         }
 
-        return (
-            <div className={styles.all}>
-                <div className={styles.ui}>
-                    <h1 className="hero__title uiText">Deploying made easy</h1>
-                    <p className="hero__subtitle">Cyclops gives you a UI containing fields you define yourself to manage your
-                        K8s workloads.</p>
-                    <p className="hero__subtitle">Give it a go!</p>
-                    <ConfigProvider
-                        theme={{
-                            token: {
-                                colorPrimary: '#fe8801',
-                            },
-                        }}
-                    >
-                        <Form labelCol={{span: '6'}}>
-                            <Form.Item
-                                label="Name"
-                                style={{ flexDirection: 'row' }}
-                            >
-                                <Input defaultValue={'nginx'} onChange={onNameChange}/>
-                            </Form.Item>
-                            <Form.Item
-                                label="Replicas"
-                                style={{display: 'block'}}
-                            >
-                                <InputNumber style={{width: '100%'}} defaultValue={'3'} onChange={onReplicasChange}/>
-                            </Form.Item>
-                            <Form.Item
-                                label="Version"
-                                style={{display: 'block'}}
-                            >
-                                <Input defaultValue={'1.14.2'} onChange={onVersionChange}/>
-                            </Form.Item>
-                            <Form.Item
-                                label="Port"
-                                style={{display: 'block'}}
-                            >
-                                <InputNumber style={{width: '100%'}} defaultValue={'80'} onChange={onPortChange}/>
-                            </Form.Item>
-                            <Form.Item
-                                label="Need service"
-                                style={{display: 'block', textAlign: 'left'}}
-                            >
-                                <Switch defaultValue={'false'} onChange={onNeedService}/>
-                            </Form.Item>
-                        </Form>
-                    </ConfigProvider>
-                </div>
-                <div className={styles.yaml}>
-                    <CodeBlock
-                        language={'yaml'}
-                        showLineNumbers={true}
-                    >
-                        {getManifest()}
-                    </CodeBlock>
-                </div>
-            </div>
-        );
+        if (n > 5) {
+            return 5;
+        }
+
+        if (n < 0) {
+            return 0;
+        }
+
+        return n;
     }
+
+    const mapPort = (n) => {
+        if (n === null || n === undefined) {
+            return 80
+        }
+
+        return n;
+    }
+
+    const k8sDplString = 'apiVersion: apps/v1\n' +
+        'kind: Deployment\n' +
+        'metadata:\n' +
+        '  name: ' + name + '\n' +
+        '  labels:\n' +
+        '    app: ' + name + '\n' +
+        'spec:\n' +
+        '  replicas: ' + mapReplicas(replicas) + '\n' +
+        '  selector:\n' +
+        '    matchLabels:\n' +
+        '      app: ' + name + '\n' +
+        '  template:\n' +
+        '    metadata:\n' +
+        '      labels:\n' +
+        '        app: ' + name + '\n' +
+        '    spec:\n' +
+        '      containers:\n' +
+        '      - name: ' + name + '\n' +
+        '        image: ' + name + ':' + version + '\n' +
+        '        ports:\n' +
+        '        - containerPort: ' + mapPort(port)
+
+    const k8sSvcString = '---\n' +
+        'apiVersion: v1\n' +
+        'kind: Service\n' +
+        'metadata:\n' +
+        '  name: ' + name + '\n' +
+        'spec:\n' +
+        '  selector:\n' +
+        '    app: ' + name + '\n' +
+        '  ports:\n' +
+        '    - protocol: TCP\n' +
+        '      port: ' + mapPort(port) + '\n' +
+        '      targetPort: 9376'
+
+    const onNameChange = (event) => {
+        setName(event.target.value)
+    }
+
+    const onReplicasChange = (replicas) => {
+        setReplicas(replicas)
+    }
+
+    const onVersionChange = (event) => {
+        setVersion(event.target.value)
+    }
+
+    const onPortChange = (port) => {
+        setPort(port)
+    }
+
+    const onNeedService = (service) => {
+        setExpose(service)
+    }
+
+    const getManifest = () => {
+        var manifest = k8sDplString;
+
+        if (expose) {
+            manifest += '\n' + k8sSvcString;
+        }
+
+        return manifest
+    }
+
+    return (
+        <div className={styles.all}>
+            <div className={styles.ui}>
+                <div>
+                    <h1 className={styles.title}>Deploying made <span style={{color: "orange"}}>easy</span></h1>
+                </div>
+                <ConfigProvider
+                    theme={{
+                        token: {
+                            colorPrimary: '#fe8801',
+                        },
+                    }}
+                >
+                    <Form labelCol={{span: '6'}} form={form}>
+                        <Form.Item
+                            label="Name"
+                            name="Name"
+                            style={{flexDirection: 'row'}}
+                        >
+                            <Input defaultValue={name} onChange={onNameChange}/>
+                        </Form.Item>
+                        <Form.Item
+                            label="Replicas"
+                            style={{display: 'block'}}
+                            name="Replicas"
+                            rules={[
+                                {
+                                    type: 'number',
+                                    min: 0,
+                                    message: 'Number of replicas must not be below 0'
+                                },
+                                {
+                                    type: 'number',
+                                    max: 5,
+                                    message: 'Number of replicas must not exceed 5'
+                                }
+                            ]}
+                        >
+                            <InputNumber style={{width: '100%'}} defaultValue={replicas} onChange={onReplicasChange}/>
+                        </Form.Item>
+                        <Form.Item
+                            label="Version"
+                            style={{display: 'block'}}
+                        >
+                            <Input defaultValue={version} onChange={onVersionChange}/>
+                        </Form.Item>
+                        <Form.Item
+                            label="Port"
+                            style={{display: 'block'}}
+                        >
+                            <InputNumber style={{width: '100%'}} defaultValue={port} onChange={onPortChange}/>
+                        </Form.Item>
+                        <Form.Item
+                            label="Expose"
+                            style={{display: 'block', textAlign: 'left'}}
+                        >
+                            <Switch defaultValue={expose} onChange={onNeedService}/>
+                        </Form.Item>
+                    </Form>
+                </ConfigProvider>
+            </div>
+            <div className={styles.yaml}>
+                <SyntaxHighlighter
+                    language="yaml"
+                    style={{
+                        "hljs-attr": {
+                            color: "#FFF"
+                        },
+                        "react-syntax-highlighter-line-number": {
+                            color: "#a7a7a7"
+                        }
+                    }}
+                    showLineNumbers={true}
+                    customStyle={{
+                        backgroundColor: "#000830",
+                        color: "#fe8801"
+                    }}
+                >
+                    {getManifest()}
+                </SyntaxHighlighter>
+            </div>
+        </div>
+    );
 }
 
 export default Comparison;
