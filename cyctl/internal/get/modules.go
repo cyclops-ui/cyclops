c0b87f5899a058f248d9dcff7e13b0c1a97ac650
Hanshal Mehta <122217807+hanshal101@users.noreply.github.com>
2024-04-22 02:47:47 +0530

    feat(cli): add get modules command in the cli

--- cyctl/internal/get/modules.go
+++ cyctl/internal/get/modules.go
@@ -6,18 +6,12 @@ import (
 	"time"
 
 	"github.com/cyclops-ui/cyclops/cyclops-ctrl/api/v1alpha1/client"
-	"github.com/ryanuber/columnize"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func ListModules(clientset *client.CyclopsV1Alpha1Client) {
 	modules, err := clientset.Modules("cyclops").List(metav1.ListOptions{})
 	if err != nil {
-		if errors.IsNotFound(err) {
-			fmt.Println("No modules found.")
-			return
-		}
 		fmt.Printf("Error listing modules: %v\n", err)
 		return
 	}
@@ -29,13 +23,9 @@ func ListModules(clientset *client.CyclopsV1Alpha1Client) {
 		}
 	}
 
-	moduleOutput := []string{"NAME" + strings.Repeat(" ", longestName-4) + " | AGE"}
+	fmt.Println("NAME" + strings.Repeat(" ", longestName-4) + " AGE")
 	for _, module := range modules {
 		age := time.Since(module.CreationTimestamp.Time).Round(time.Second)
-		moduleOutput = append(moduleOutput, fmt.Sprintf("%s"+strings.Repeat(" ", longestName-len(module.Name))+" | %s", module.Name, age.String()))
+		fmt.Printf("%s"+strings.Repeat(" ", longestName-len(module.Name))+" %s\n", module.Name, age.String())
 	}
-
-	moduleResult := columnize.SimpleFormat(moduleOutput)
-	fmt.Println(moduleResult)
-
 }
