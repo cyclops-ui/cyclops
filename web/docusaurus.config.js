dc9f9f5b0001fabf43df259f697fb3249d38c6f1
petar-cvit <petar.cvitanovic@gmail.com>
2024-04-22 13:03:40 +0200

    ♻️ update social card

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/which-would-you-prefer.png",
+      image: "img/cyclops-social-card.png",
       navbar: {
         style: 'dark',
         logo: {
