576363e124f69a1022f91ba3b8f15a2777753b1f
Hanshal Mehta <122217807+hanshal101@users.noreply.github.com>
2024-05-05 15:59:02 +0530

    feat(backend): add prometheus for cyclops

--- cyclops-ctrl/prometheus/handler.go
+++ cyclops-ctrl/prometheus/handler.go
@@ -1,19 +1,27 @@
 package prometheusHandler
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
+var setupLog = ctrl.Log.WithName("setup")
+
 var moduleQueued = prometheus.NewGauge(prometheus.GaugeOpts{
-	Name:      "Modules",
+	Name:      "modules_deployed",
 	Help:      "No of modules Inc or Dec",
 	Namespace: "cyclops",
 })
 
 func init() {
-	prometheus.MustRegister(moduleQueued)
+	if err := prometheus.MustRegister(moduleQueued); err != nil {
+		setupLog.Error(err, "unable to connect prometheus")
+		os.Exit(1)
+	}
 }
 
 func IncModule() {
