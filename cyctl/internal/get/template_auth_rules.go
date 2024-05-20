3c1a1416b221f4d4a8d8a38e4dd80c483b6c2751
siddhantprateek <siddhantprateek@gmail.com>
2024-05-20 18:46:25 +0530

    ft(cyctl): Get template auth rules functionalility

---
+++ cyctl/internal/get/template_auth_rules.go
@@ -0,0 +1,33 @@
+package get
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/cyclops-ui/cyclops/cyclops-ctrl/api/v1alpha1/client"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func ListTemplateAuthRules(clientset *client.CyclopsV1Alpha1Client) {
+	templates, err := clientset.TemplateAuthRules("cyclops").List(metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("Error listing templates: %v\n", err)
+		return
+	}
+
+	longestName := 20 // minimum column width
+	for _, template := range templates {
+		if len(template.Name) > longestName {
+			longestName = len(template.Name)
+		}
+	}
+
+	headerSpacing := max(0, longestName-4)
+	fmt.Println("NAME" + strings.Repeat(" ", headerSpacing) + " AGE")
+	for _, template := range templates {
+		age := time.Since(template.CreationTimestamp.Time).Round(time.Second)
+		nameSpacing := max(0, longestName-len(template.Name))
+		fmt.Printf("%s"+strings.Repeat(" ", nameSpacing)+" %s\n", template.Name, age.String())
+	}
+}
