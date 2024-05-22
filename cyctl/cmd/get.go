f0abe24d910680c0d78df8cdfef36ab1cd1f7131
siddhantprateek <siddhantprateek@gmail.com>
2024-05-22 14:37:06 +0530

    ft(cyctl): get and delete now have available subcommands

--- cyctl/cmd/get.go
+++ cyctl/cmd/get.go
@@ -1,39 +1,34 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/cyclops-ui/cycops-cyctl/internal/get"
-	"github.com/cyclops-ui/cycops-cyctl/internal/kubeconfig"
 	"github.com/spf13/cobra"
 )
 
-var getCMD = &cobra.Command{
-	Use:   "get",
-	Short: "Retrieve custom resources like modules, templates, and templateauthrules",
-	Long:  "getLong",
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) == 0 {
-			fmt.Println(cmd.Long)
-			_ = cmd.Usage()
-			return
-		}
+var (
+	getExample = `
+	# List all modules in ps output format
+	cyctl get modules 
+ 
+	# List all templates available in template store in ps output format
+	cyctl get templates
+ 
+	# List all template auth rules available in ps format
+	cyctl get templateauthrules`
+)
 
-		getCommand := args[0]
-		switch getCommand {
-		case "modules", "module":
-			get.ListModules(kubeconfig.Moduleset)
-		case "templates", "template":
-			get.ListTemplateStore(kubeconfig.Moduleset)
-		case "templateauthrules", "templateauthrule":
-			get.ListTemplateAuthRules(kubeconfig.Moduleset)
-		default:
-			fmt.Println("Give the correct resource name")
-		}
-	},
+var getCMD = &cobra.Command{
+	Use:     "get",
+	Short:   "Retrieve custom resources like modules, templates, and templateauthrules",
+	Long:    "Retrieve custom resources like modules, templates, and templateauthrules",
+	Example: getExample,
+	// Run:     func(cmd *cobra.Command, args []string) {},
 }
 
 func init() {
+	getCMD.AddCommand(get.ListModule)
+	getCMD.AddCommand(get.ListTemplate)
+	getCMD.AddCommand(get.ListTemplateAuthRules)
+
 	RootCmd.AddCommand(getCMD)
 }
