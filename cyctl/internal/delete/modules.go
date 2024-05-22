8511f79542cfb5c881d196b657998be59fe1a31e
siddhantprateek <siddhantprateek@gmail.com>
2024-05-22 14:29:15 +0530

    ft(cyctl): refactored module get/delete and added usage examples & description

--- cyctl/internal/delete/modules.go
+++ cyctl/internal/delete/modules.go
@@ -4,10 +4,21 @@ import (
 	"fmt"
 
 	"github.com/cyclops-ui/cyclops/cyclops-ctrl/api/v1alpha1/client"
+	"github.com/cyclops-ui/cycops-cyctl/internal/kubeconfig"
+	"github.com/spf13/cobra"
+)
+
+var (
+	deleteModuleExample = ` 
+	# Delete a single module
+	cyctl delete modules module1
+ 
+	# Delete multiple modules
+	cyctl delete modules module1 module2 module3`
 )
 
 // DeleteModules deletes a specified module from the Cyclops API.
-func DeleteModules(clientset *client.CyclopsV1Alpha1Client, moduleNames []string) {
+func deleteModules(clientset *client.CyclopsV1Alpha1Client, moduleNames []string) {
 	if len(moduleNames) == 0 {
 		fmt.Println("Error: module names cannot be empty")
 		return
@@ -22,3 +33,17 @@ func DeleteModules(clientset *client.CyclopsV1Alpha1Client, moduleNames []string
 		}
 	}
 }
+
+var (
+	DeleteModule = &cobra.Command{
+		Use:     "modules [module_name]",
+		Short:   "Delete one or more modules",
+		Long:    "The delete modules command allows you to remove one or more modules from the Cyclops API.",
+		Example: deleteModuleExample,
+		Aliases: []string{"module"},
+		Args:    cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			deleteModules(kubeconfig.Moduleset, args)
+		},
+	}
+)
