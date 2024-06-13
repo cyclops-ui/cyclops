bb9723007b95572173111bfc94a992b05d3b63b9
thebigbone <pacman@duck.com>
2024-06-13 17:17:41 +0530

    use k8s api for port forwarding

--- cyctl/cmd/serve.go
+++ cyctl/cmd/serve.go
@@ -1,27 +1,111 @@
 package cmd
 
 import (
+	"bytes"
+	"context"
+	"flag"
 	"fmt"
-	"os"
-	"os/exec"
-
 	"github.com/spf13/cobra"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"path/filepath"
+	"strings"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
+	"k8s.io/client-go/util/homedir"
 )
 
 var serveCMD = &cobra.Command{
 	Use:   "serve",
-	Short: "start the cyclops UI",
-	Long:  "start the cyclops UI by forwarding the cyclops UI service's port to a local port",
+	Short: "Start the Cyclops UI",
+	Long:  "Start the Cyclops UI by forwarding the Cyclops UI service's port to a local port",
 	Run: func(cmd *cobra.Command, args []string) {
-		command := exec.Command("kubectl", "port-forward", "svc/cyclops-ui", "3000:3000", "-n", "cyclops")
-		command.Stdout = os.Stdout
-		command.Stderr = os.Stderr
-		err := command.Run()
-		if err != nil {
-			fmt.Printf("error executing command: %s\n", err.Error())
-			os.Exit(1)
-		}
+		startPortForwarding()
 	},
+}
+
+func startPortForwarding() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	service, err := clientset.CoreV1().Services("cyclops").Get(context.TODO(), "cyclops-ui", metav1.GetOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	podList, err := clientset.CoreV1().Pods("cyclops").List(context.TODO(), metav1.ListOptions{
+		LabelSelector: labels.Set(service.Spec.Selector).String(),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(podList.Items) == 0 {
+		log.Fatal("no pods found for service my-service in namespace default")
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	podIndex := rand.Intn(len(podList.Items))
+	podName := podList.Items[podIndex].Name
+
+	roundTripper, upgrader, err := spdy.RoundTripperFor(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path := fmt.Sprintf("/api/v1/namespaces/%s/pods/%s/portforward", "cyclops", podName)
+	hostIP := strings.TrimLeft(config.Host, "htps:/")
+	serverURL := url.URL{Scheme: "https", Path: path, Host: hostIP}
+
+	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: roundTripper}, http.MethodPost, &serverURL)
+
+	ports := []string{"3000:80"}
+
+	stopChan, readyChan := make(chan struct{}, 1), make(chan struct{}, 1)
+	out, errOut := new(bytes.Buffer), new(bytes.Buffer)
+
+	forwarder, err := portforward.New(dialer, ports, stopChan, readyChan, out, errOut)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for range readyChan {
+		}
+		if len(errOut.String()) != 0 {
+			log.Fatal(errOut.String())
+		} else if len(out.String()) != 0 {
+			fmt.Println(out.String())
+		}
+	}()
+
+	if err = forwarder.ForwardPorts(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Port forwarding stopped")
 }
 
 func init() {
