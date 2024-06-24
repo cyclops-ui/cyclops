e7c7c26f3ca1d6bd266bc022e71b346837d0324f
Sukuna <90980311+samarsrivastav@users.noreply.github.com>
2024-06-24 22:53:04 +0530

    Update ModuleDetails.tsx

--- cyclops-ui/src/components/pages/ModuleDetails.tsx
+++ cyclops-ui/src/components/pages/ModuleDetails.tsx
@@ -29,6 +29,7 @@ import "./custom.css";
 import "ace-builds/src-noconflict/mode-jsx";
 import ReactAce from "react-ace";
 import Deployment from "../k8s-resources/Deployment";
+import CronJob from "../k8s-resources/CronJob";
 import DaemonSet from "../k8s-resources/DaemonSet";
 import StatefulSet from "../k8s-resources/StatefulSet";
 import Pod from "../k8s-resources/Pod";
