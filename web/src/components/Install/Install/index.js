2230fa7fa063774921193442b60aa7b0200a7420
KaradzaJuraj <101581637+KaradzaJuraj@users.noreply.github.com>
2024-06-26 18:11:18 +0200

    ⬇️ update install intstructions (#376)
    ⬇️ update install intstructions

--- web/src/components/Install/Install/index.js
+++ web/src/components/Install/Install/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import styles from './styles.module.css';
+import React from "react";
+import styles from "./styles.module.css";
 
 import CodeBlockString from "@docusaurus/theme-classic/lib/theme/CodeBlock/Content/String";
 
 const InstallCmd = () => {
-    return (
-        <center className={styles.command}>
-            <h2 className={styles.commandDesc}>
-                Install it with a single command
-            </h2>
-            <CodeBlockString language={"sh"}>
-                {"kubectl apply -f https://raw.githubusercontent.com/cyclops-ui/cyclops/v0.6.2/install/cyclops-install.yaml && \n" +
-                    "kubectl apply -f https://raw.githubusercontent.com/cyclops-ui/cyclops/v0.6.2/install/demo-templates.yaml"}
-            </CodeBlockString>
-        </center>
-    );
-}
+  return (
+    <center className={styles.command}>
+      <h2 className={styles.commandDesc}>Install it with a single command</h2>
+      <CodeBlockString language={"sh"}>
+        {"kubectl apply -f https://raw.githubusercontent.com/cyclops-ui/cyclops/v0.7.1/install/cyclops-install.yaml && \n" +
+          "kubectl apply -f https://raw.githubusercontent.com/cyclops-ui/cyclops/v0.7.1/install/demo-templates.yaml"}
+      </CodeBlockString>
+    </center>
+  );
+};
 
 export default InstallCmd;
