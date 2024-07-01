790b7e351e54e6608d44aab8b73465ea23114e45
Sukuna <90980311+samarsrivastav@users.noreply.github.com>
2024-07-01 14:49:18 +0530

    🔨 representation jobs (#386)
    * Create Job.tsx
    
    * Update ModuleDetails.tsx
    
    * Update ModuleDetails.tsx
    
    * Update ModuleDetails.tsx

--- cyclops-ui/src/components/pages/ModuleDetails.tsx
+++ cyclops-ui/src/components/pages/ModuleDetails.tsx
@@ -30,6 +30,7 @@ import "ace-builds/src-noconflict/mode-jsx";
 import ReactAce from "react-ace";
 import Deployment from "../k8s-resources/Deployment";
 import CronJob from "../k8s-resources/CronJob";
+import Job from "../k8s-resources/Job";
 import DaemonSet from "../k8s-resources/DaemonSet";
 import StatefulSet from "../k8s-resources/StatefulSet";
 import Pod from "../k8s-resources/Pod";
@@ -382,6 +383,11 @@ const ModuleDetails = () => {
       case "CronJob":
         resourceDetails = (
           <CronJob name={resource.name} namespace={resource.namespace} />
+        );
+        break;
+      case "Job":
+        resourceDetails = (
+          <Job name={resource.name} namespace={resource.namespace} />
         );
         break;
       case "DaemonSet":
