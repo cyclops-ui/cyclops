e772b54f00013dc74da8815b4b2576d2a1d02959
Petar Cvitanović <72022639+petar-cvit@users.noreply.github.com>
2024-07-18 13:25:49 +0200

    💀 map gvrs on module edit (#441)

--- cyclops-ctrl/internal/controller/modules.go
+++ cyclops-ctrl/internal/controller/modules.go
@@ -288,6 +288,7 @@ func (m *Modules) UpdateModule(ctx *gin.Context) {
 	module.SetResourceVersion(curr.GetResourceVersion())
 
 	module.Status.TemplateResolvedVersion = request.Template.ResolvedVersion
+	module.Status.ManagedGVRs = curr.Status.ManagedGVRs
 	result, err := m.kubernetesClient.UpdateModuleStatus(&module)
 	if err != nil {
 		fmt.Println(err)
