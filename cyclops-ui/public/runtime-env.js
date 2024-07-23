128c3757c349a777ed1aceb603917ce45c2d80d5
siddhantprateek <siddhantprateek@gmail.com>
2024-07-23 16:22:38 +0530

    consistent env var for authorization

--- cyclops-ui/public/runtime-env.js
+++ cyclops-ui/public/runtime-env.js
@@ -1,5 +1,5 @@
 window.__RUNTIME_CONFIG__ = {
   NODE_ENV: "development",
   REACT_APP_CYCLOPS_CTRL_HOST: "http://localhost:8888",
-  REACT_APP_CYCLOPS_AUTH: "disable",
+  REACT_APP_CYCLOPS_AUTHORIZATION: "enabled",
 };
