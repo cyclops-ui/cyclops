3fd7194bcd2a855036bda88a8944a70293737d96
petar-cvit <petar.cvitanovic@gmail.com>
2024-11-16 19:04:39 +0100

    bump to v0.15.2

--- web/src/components/Install/Install/index.js
+++ web/src/components/Install/Install/index.js
@@ -4,17 +4,17 @@ import styles from "./styles.module.css";
 import CodeBlockString from "@docusaurus/theme-classic/lib/theme/CodeBlock/Content/String";
 
 const InstallCmd = () => {
-    return (
-        <center className={styles.command}>
-            <h2 className={styles.commandDesc}>
-                Install it with a single command
-            </h2>
-            <CodeBlockString language={"sh"}>
-                {"kubectl apply -f https://raw.githubusercontent.com/cyclops-ui/cyclops/v0.15.2/install/cyclops-install.yaml && \n" +
-                    "kubectl apply -f https://raw.githubusercontent.com/cyclops-ui/cyclops/v0.15.2/install/demo-templates.yaml"}
-            </CodeBlockString>
-        </center>
-    );
+  return (
+    <center className={styles.command}>
+      <h2 className={styles.commandDesc}>
+        Install it with a single command
+      </h2>
+      <CodeBlockString language={"sh"}>
+        {"kubectl apply -f https://raw.githubusercontent.com/cyclops-ui/cyclops/v0.15.2/install/cyclops-install.yaml && \n" +
+          "kubectl apply -f https://raw.githubusercontent.com/cyclops-ui/cyclops/v0.15.2/install/demo-templates.yaml"}
+      </CodeBlockString>
+    </center>
+  );
 };
 
 export default InstallCmd;
