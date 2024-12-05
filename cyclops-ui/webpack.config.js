d465a7ad78e63db5450b737bca3a458f482c2316
petar-cvit <petar.cvitanovic@gmail.com>
2024-12-05 18:30:19 +0100

    webpack config

---
+++ cyclops-ui/webpack.config.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+    sourceMapFilename: "[file].map",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: {
+          loader: "esbuild-loader",
+          options: {
+            loader: "tsx",
+            target: "esnext",
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.module\.css$/,
+        use: [
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8192,
+          },
+        },
+      },
+      {
+        test: /\.json$/,
+        type: "json",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new webpack.DefinePlugin({
+      "window.__RUNTIME_CONFIG__": JSON.stringify({
+        REACT_APP_VERSION: "0.0.0",
+      }),
+    }),
+  ],
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+    antd: "antd",
+  },
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+  devtool: "source-map",
+};
