8a9a0c2136baa2e71d9eb13121768b94b36b2984
petar-cvit <petar.cvitanovic@gmail.com>
2025-04-27 20:23:30 +0200

    indent data

--- web/src/components/Testimonials/index.js
+++ web/src/components/Testimonials/index.js
@@ -23,81 +23,81 @@ const Testimonials = () => {
             position: "SRE",
             testimonial: "Cyclops utilizes Helm charts for application deployment, making Kubernetes more accessible to users with different levels of expertise."
         },
-          {
-              name: "Adolfo",
-              position: "SRE/DevOps",
-              testimonial: "I enjoyed using cyclops because of it's simplicity and enhanced observability of kubernetes clusters as well as the seamless integrations with tools like  prometheus and Grafana",
-          },
-          {
-              name: "Hanzel",
-              position: "DevOps Engineer",
-              testimonial: "I've been using Cyclops for a few months now in my projects to streamline the creation of applications in Kubernetes, and the experience has been excellent. I find the interface very intuitive, and the integration with Helm has significantly simplified the deployment process, allowing me to focus more on development rather than configuration. I've also recommended Cyclops to several friends who don't have extensive knowledge of Kubernetes, and they've found the tool incredibly useful for creating applications easily and without complications. I truly appreciate the work you and your team are doing with Cyclops, and I'm very excited to see how it evolves in the future.",
-          },
-          {
-              name: "Rodrigo",
-              position: "Senior DevOps Engineer",
-              testimonial: "I actually installed it and showed it in a presentation about how to create Internal Developers Portal. It was an example on how to use a ready to use product instead of using a framework like backstage. I found it very easy to understand and use, congratulations on getting it there 👏",
-          },
-      {
-        name: "Jesus",
-        position: "Software Developer",
-        testimonial: "Cyclops does an amazing job of simplifying the complexities of Kubernetes while boosting developer efficiency.",
-      },
-      {
-        name: "Hanshal",
-        position: "DevOps",
-        testimonial: "It's a really nice tool to manage and deploy Kubernetes.",
-      },
-      {
-        name: "Jurica",
-        position: "Software Engineer",
-        testimonial: "Love the tool! Helps me navigate the k8s mess a lot more easily.",
-      },
-      {
-        name: "Ajay",
-        position: "Developer/DevOps Engineer",
-        testimonial: "Amazing! Easy to use! A must-try tool to simplify their k8s operations! Loved it!",
-      },
-      {
-        name: "Anmol",
-        position: "Software Developer (Blockhain Dev)",
-        testimonial: "I really like the idea of Cyclops. It is so much easier for users who dont know or need to know about k8s. Why did this not exist before?",
-      },
-      {
-        name: "Raj",
-        position: "Software Developer",
-        testimonial: "I'm really impressed with the Cyclops UI so far. The community seems active and supportive, which is fantastic.",
-      },
-          {
-              name: "David",
-              position: "SRE/DevOps",
-              testimonial: "I really like it. I've been using it in my personal lab environments, and it's been excellent. I've also recommended it to a couple of friends who manage Kubernetes, and they've found it very useful as well.",
-          },
-          {
-              name: "Sergio",
-              position: "SRE",
-              testimonial: "Great project that cyclops UI is really like (as an SRE) a straighfoward way to create modules for dev teams without really in more complex ways. awesome work for the community",
-          },
-          {
-              name: "Prince",
-              position: "Software Engineer",
-              testimonial: "I'm impressed with its functionality. It worked seamlessly with a Minikube cluster, and I particularly loved the edit feature for modifying images and replicas. The rollback feature also stood out as a fantastic addition, making deployments much easier to manage.",
-          },
-          {
-              name: "Edgar",
-              position: "DevOps",
-              testimonial: "I did a quick demo to my students to show they how easy is deploy applications using cyclops. (It's a technology institute, and I teach classes for a DevOps bootcamp.)",
-          },
-          {
-              name: "Kaan",
-              position: "R&D Engineer",
-              testimonial: "I'm impressed by how it simplifies Kubernetes management and service orchestration. As a small company without a dedicated DevOps team, we understand the importance of user-friendly tools that don't require extensive specialized knowledge. What makes Cyclops perfect for us is its ability to simplify the management of various services. We're working on implementing CNCF products like APISIX, Keycloak, OpenFGA, Prometheus, and Grafana, and Cyclops could be a game-changer in how we handle these integrations.",
-          },
-          {
-              name: "Ashish",
-              position: "Engineer",
-              testimonial: "Cyclops is awesome, makes Kubernetes easy for devs.",
-          }
+        {
+            name: "Adolfo",
+            position: "SRE/DevOps",
+            testimonial: "I enjoyed using cyclops because of it's simplicity and enhanced observability of kubernetes clusters as well as the seamless integrations with tools like  prometheus and Grafana",
+        },
+        {
+            name: "Hanzel",
+            position: "DevOps Engineer",
+            testimonial: "I've been using Cyclops for a few months now in my projects to streamline the creation of applications in Kubernetes, and the experience has been excellent. I find the interface very intuitive, and the integration with Helm has significantly simplified the deployment process, allowing me to focus more on development rather than configuration. I've also recommended Cyclops to several friends who don't have extensive knowledge of Kubernetes, and they've found the tool incredibly useful for creating applications easily and without complications. I truly appreciate the work you and your team are doing with Cyclops, and I'm very excited to see how it evolves in the future.",
+        },
+        {
+            name: "Rodrigo",
+            position: "Senior DevOps Engineer",
+            testimonial: "I actually installed it and showed it in a presentation about how to create Internal Developers Portal. It was an example on how to use a ready to use product instead of using a framework like backstage. I found it very easy to understand and use, congratulations on getting it there 👏",
+        },
+        {
+          name: "Jesus",
+          position: "Software Developer",
+          testimonial: "Cyclops does an amazing job of simplifying the complexities of Kubernetes while boosting developer efficiency.",
+        },
+        {
+          name: "Hanshal",
+          position: "DevOps",
+          testimonial: "It's a really nice tool to manage and deploy Kubernetes.",
+        },
+        {
+          name: "Jurica",
+          position: "Software Engineer",
+          testimonial: "Love the tool! Helps me navigate the k8s mess a lot more easily.",
+        },
+        {
+          name: "Ajay",
+          position: "Developer/DevOps Engineer",
+          testimonial: "Amazing! Easy to use! A must-try tool to simplify their k8s operations! Loved it!",
+        },
+        {
+          name: "Anmol",
+          position: "Software Developer (Blockhain Dev)",
+          testimonial: "I really like the idea of Cyclops. It is so much easier for users who dont know or need to know about k8s. Why did this not exist before?",
+        },
+        {
+          name: "Raj",
+          position: "Software Developer",
+          testimonial: "I'm really impressed with the Cyclops UI so far. The community seems active and supportive, which is fantastic.",
+        },
+        {
+            name: "David",
+            position: "SRE/DevOps",
+            testimonial: "I really like it. I've been using it in my personal lab environments, and it's been excellent. I've also recommended it to a couple of friends who manage Kubernetes, and they've found it very useful as well.",
+        },
+        {
+            name: "Sergio",
+            position: "SRE",
+            testimonial: "Great project that cyclops UI is really like (as an SRE) a straighfoward way to create modules for dev teams without really in more complex ways. awesome work for the community",
+        },
+        {
+            name: "Prince",
+            position: "Software Engineer",
+            testimonial: "I'm impressed with its functionality. It worked seamlessly with a Minikube cluster, and I particularly loved the edit feature for modifying images and replicas. The rollback feature also stood out as a fantastic addition, making deployments much easier to manage.",
+        },
+        {
+            name: "Edgar",
+            position: "DevOps",
+            testimonial: "I did a quick demo to my students to show they how easy is deploy applications using cyclops. (It's a technology institute, and I teach classes for a DevOps bootcamp.)",
+        },
+        {
+            name: "Kaan",
+            position: "R&D Engineer",
+            testimonial: "I'm impressed by how it simplifies Kubernetes management and service orchestration. As a small company without a dedicated DevOps team, we understand the importance of user-friendly tools that don't require extensive specialized knowledge. What makes Cyclops perfect for us is its ability to simplify the management of various services. We're working on implementing CNCF products like APISIX, Keycloak, OpenFGA, Prometheus, and Grafana, and Cyclops could be a game-changer in how we handle these integrations.",
+        },
+        {
+            name: "Ashish",
+            position: "Engineer",
+            testimonial: "Cyclops is awesome, makes Kubernetes easy for devs.",
+        }
         ]
 
     const column1Testimonials = testimonials.slice(0, 6);
