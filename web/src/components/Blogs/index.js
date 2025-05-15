0f204da43a46212f044ef10af6592cc4a956347b
KaradzaJuraj <101581637+KaradzaJuraj@users.noreply.github.com>
2025-05-15 15:00:55 +0200

    🚩 [blog] Signs you might need an IDP (#835)

--- web/src/components/Blogs/index.js
+++ web/src/components/Blogs/index.js
@@ -23,6 +23,19 @@ const Blogs = () => {
         <Row gutter={[16, 16]}>
           <Col xs={{ span: 20, offset: 2 }} md={{ span: 6, offset: 3 }}>
             <Blog
+              title={"Signs You Might Need a Developer Platform"}
+              description={
+                "We put together a lighthearted guide, told through memes, to help you spot the signs early..."
+              }
+              blogLink={
+                "https://cyclops-ui.com/blog/2025/05/15/idp-signs"
+              }
+              avatar={"https://github.com/KaradzaJuraj.png"}
+              banner={"/img/2025-05-15-idp-signs/cover.png"}
+            />
+          </Col>
+          <Col xs={{ span: 20, offset: 2 }} md={{ span: 6, offset: 0 }}>
+            <Blog
               title={"Vibe Coding on Kubernetes with Cyclops MCP"}
               description={
                 "You want your infrastructure to be stable, and vibe coding isn't exactly known for stability..."
@@ -45,19 +58,6 @@ const Blogs = () => {
               }
               avatar={"https://github.com/KaradzaJuraj.png"}
               banner={"/img/2025-04-24-golden-paths/cover.png"}
-            />
-          </Col>
-          <Col xs={{ span: 20, offset: 2 }} md={{ span: 6, offset: 0 }}>
-            <Blog
-              title={"From Helm Chart to Developer UI in 5 Minutes"}
-              description={
-                "Helm is great… …until you give it to a developer."
-              }
-              blogLink={
-                "https://cyclops-ui.com/blog/2025/04/03/helm-to-idp-in-five"
-              }
-              avatar={"https://github.com/KaradzaJuraj.png"}
-              banner={"/img/2025-04-03-helm-to-idp-in-five/cover.png"}
             />
           </Col>
         </Row>
